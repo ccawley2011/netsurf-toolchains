8913365fda63d1eeab180cca1f308ae36b5f0129
John Mark Bell <jmb@netsurf-browser.org>
2010-12-29 02:39:59 +0000

    Add glob.h and a stub implementation thereof
    svn path=/toolchains/; revision=11130

--- m68k-unknown-amigaos/recipes/files/clib2/stubs.c
+++ m68k-unknown-amigaos/recipes/files/clib2/stubs.c
@@ -1,3 +1,4 @@
+#include <glob.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -23,5 +24,16 @@ int pipe(int pipefd[2])
 	__set_errno(ENFILE);
 
 	return -1;
+}
+
+int glob(const char *pattern, int flags,
+	int (*errfunc) (const char *epath, int eerrno),
+	glob_t *pglob)
+{
+	return GLOB_NOMATCH;
+}
+
+void globfree(glob_t *pglob)
+{
 }
 
